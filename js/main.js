afb1f80fd7dd48a95d70ca2ff1401421e9f71a98
Daulet <51912820+Daulet070@users.noreply.github.com>
2022-06-06 01:14:46 +0300

    Fixed linter errors

--- js/main.js
+++ js/main.js
@@ -1,29 +1,29 @@
 const isValid = (min, max, cut) => {
-    const start = 0;
-    const isNumber = (num) => !isNaN(parseFloat(num)) && isFinite(num);
-    
-    const result = (
-        min < start 
-        || max <= start
-        || min >= max
-        || cut < start
-        || !isNumber(min)
-        || !isNumber(max)
-        || !isNumber(cut)
-        || !Number.isInteger(cut)
-    ) ? false : true;
-    
-    return result;
-}
+  const start = 0;
+  const isNumber = (num) => !isNaN(parseFloat(num)) && isFinite(num);
 
-const randomGenerator = (min, max, cut = 0) => {
-    
-    const result = isValid(min, max, cut) 
-    ? Number((Math.random() * (max - min + 1) + min).toFixed(cut))
-    : console.warn('Недопустимые значения');
-    
-    return result
+  if (
+    min < start ||
+    max <= start ||
+    min >= max ||
+    cut < start ||
+    !isNumber(min) ||
+    !isNumber(max) ||
+    !isNumber(cut) ||
+    !Number.isInteger(cut)
+  ) {
+    return false;
+  }
+  return true;
 };
 
-console.log(randomGenerator(1, 2)); 
-console.log(randomGenerator(1.1, 1.2, 3));
+const randomGenerator = (min, max, cut = 0) => {
+  const result = isValid(min, max, cut)
+    ? Number((Math.random() * (max - min + 1) + min).toFixed(cut))
+    : 'Недопустимые значения';
+
+  return result;
+};
+
+randomGenerator(1, 2);
+randomGenerator(1.1, 1.2, 3);
