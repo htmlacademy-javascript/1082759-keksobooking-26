cdbd6a3496736b16aa71946831136d81c721463c
Daulet <51912820+Daulet070@users.noreply.github.com>
2022-06-20 17:59:10 +0300

    Fix: added new random func and optimisations

--- js/utils.js
+++ js/utils.js
@@ -1,8 +1,8 @@
-export const isValid = (min, max, cut = 0) => {
+const isValid = (min, max, cut = 0) => {
   const START_VALUE = 0;
   const isNumber = (num) => !isNaN(parseFloat(num)) && isFinite(num);
 
-  if (
+  return !(
     min < START_VALUE ||
     max <= START_VALUE ||
     min >= max ||
@@ -10,28 +10,20 @@ export const isValid = (min, max, cut = 0) => {
     !isNumber(min) ||
     !isNumber(max) ||
     !isNumber(cut) ||
-    !Number.isInteger(cut)
-  ) {
-    return false;
-  }
-  return true;
+    !Number.isInteger(cut));
 };
 
-export const getRandomGeneratorInt = (min, max) => {
-  const result = isValid(min, max)
-    ? Math.floor(Math.random() * (max - min + 1) + min)
-    : 'Недопустимые значения';
+export const getRandomGeneratorInt = (min, max) => isValid(min, max)
+  ? Math.floor(Math.random() * (max - min + 1) + min)
+  : 'Недопустимые значения';
 
-  return result;
+export const getRandomGeneratorFloat = (min, max, cut) => isValid(min, max, cut)
+  ? Number((Math.random() * (max - min + 1) + min).toFixed(cut))
+  : 'Недопустимые значения';
+
+export const getRandomItems = (items) => items[getRandomGeneratorInt(0, items.length - 1)];
+
+export const getRandomArr = (items) => {
+  const newArrLength = getRandomGeneratorInt(0, items.length - 1);
+  return items.slice(newArrLength);
 };
-
-export const getRandomGeneratorFloat = (min, max, cut) => {
-  const result = isValid(min, max, cut)
-    ? Number((Math.random() * (max - min + 1) + min).toFixed(cut))
-    : 'Недопустимые значения';
-
-  return result;
-};
-
-export const getRandomItems = (items) =>
-  items[getRandomGeneratorInt(0, items.length - 1)];
