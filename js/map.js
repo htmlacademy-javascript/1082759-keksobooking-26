38d393e60baea0bf90a2036976598b33ea6ced8d
Daulet <51912820+Daulet070@users.noreply.github.com>
2022-07-24 20:50:48 +0300

    Changed: refactored, added map reset functions

--- js/map.js
+++ js/map.js
@@ -1,76 +1,75 @@
+import { MAIN_COORDINATES } from './housing-ads-data.js';
 import { offerCard } from './offer-card-template.js';
 import { pageState } from './page-state.js';
 
 const addressElem = document.querySelector('#address');
-
-const mainCoordinates = {
-  lat: 35.652832,
-  lng: 139.839478
-};
-const zoom = 8;
+const COPY_TXT = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+const ZOOM = 8;
 const mapLayerUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-const mapLayerOtions = {
-  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+const mapLayerOtions = { attribution: COPY_TXT };
+
+pageState('inactive');
+const iconParams = (url, size, anchor) => ({ iconUrl: url, iconSize: size, iconAnchor: anchor });
+
+const mainPinIcon = L.icon(iconParams('./img/main-pin.svg', [52, 52], [26, 52]));
+const pinIcon = L.icon(iconParams('./img/pin.svg', [40, 40], [20, 40]));
+
+const mapCanvas = L.map('map-canvas').on('load', () => pageState('active'));
+const mapView = mapCanvas.setView(MAIN_COORDINATES, ZOOM);
+const markerGroup = L.layerGroup().addTo(mapView);
+const mapLayer = L.tileLayer(mapLayerUrl, mapLayerOtions);
+
+const mainMarkerOptions = {
+  draggable: true,
+  icon: mainPinIcon
 };
 
-const iconParams = (url, size, anchor) => ({ iconUrl: url, iconSize: size, iconAnchor: anchor });
+const mainMarker = L.marker(MAIN_COORDINATES, mainMarkerOptions);
+
+mainMarker.addTo(mapView);
+
+addressElem.value = `${MAIN_COORDINATES.lat}, ${MAIN_COORDINATES.lng}`;
+
+mainMarker.on('move', (evt) => {
+
+  const { lat, lng } = evt.target.getLatLng();
+
+  addressElem.value = `${lat.toFixed(5)}, ${lng.toFixed(5)}`;
+});
+
+const createMarker = (point) => {
+
+  const iconOptions = {
+    draggable: false,
+    icon: pinIcon
+  };
+  const offerPopup = offerCard(point);
+
+  const marker = L.marker(point.location, iconOptions);
+
+  marker
+    .addTo(markerGroup)
+    .bindPopup(offerPopup);
+
+};
 
 const getMap = (data) => {
 
-  const mainPinIcon = L.icon(iconParams('./img/main-pin.svg', [52, 52], [26, 52]));
-  const pinIcon = L.icon(iconParams('./img/pin.svg', [40, 40], [20, 40]));
-
-  const map = L.map('map-canvas');
-  map.on('load', () => pageState('active'));
-
-  const mapView = map.setView(mainCoordinates, zoom);
-
-  const mapLayer = L.tileLayer(mapLayerUrl, mapLayerOtions);
   mapLayer.addTo(mapView);
-
-  const getMainMarker = (mainMap) => {
-
-    const iconOptions = {
-      draggable: true,
-      icon: mainPinIcon
-    };
-
-    const marker = L.marker(mainCoordinates, iconOptions);
-
-    marker.addTo(mainMap);
-
-    addressElem.value = `${mainCoordinates.lat}, ${mainCoordinates.lng}`;
-
-    marker.on('move', (evt) => {
-
-      const { lat, lng } = evt.target.getLatLng();
-
-      addressElem.value = `${lat.toFixed(5)}, ${lng.toFixed(5)}`;
-    });
-
-    return marker;
-  };
-
-  getMainMarker(mapView);
-
-  const markerGroup = L.layerGroup().addTo(mapView);
-
-  const createMarker = (point) => {
-
-    const offerPopup = offerCard(point);
-    const iconOptions = {
-      draggable: true,
-      icon: pinIcon
-    };
-
-    const marker = L.marker(point.location, iconOptions);
-
-    marker
-      .addTo(markerGroup)
-      .bindPopup(offerPopup);
-  };
 
   data.forEach((point) => createMarker(point));
 };
 
-export { getMap };
+const resetMap = (coordinates = MAIN_COORDINATES, zoom = ZOOM) => {
+
+  mapCanvas.setView(coordinates, zoom);
+  mapCanvas.closePopup();
+  mainMarker.setLatLng(coordinates);
+  addressElem.value = `${coordinates.lat.toFixed(5)}, ${coordinates.lng.toFixed(5)}`;
+};
+
+const removeMarkers = () => {
+  markerGroup.clearLayers();
+};
+
+export { getMap, removeMarkers, resetMap };
